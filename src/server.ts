b0f752d31a0095f87aa120fba8d5f23838b1e706
Nicolas Lohan <nicolauraujo@gmail.com>
2025-08-03 15:33:07 -0300

    fix: environment vars in docker compose

--- src/server.ts
+++ src/server.ts
@@ -117,7 +117,7 @@ async function startServer() {
 
 
 
-    app.listen({ port: env.PORT }).then(() => {
+    app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
         console.log(`HTTP server running on port ${env.PORT}`)
     })
 }
